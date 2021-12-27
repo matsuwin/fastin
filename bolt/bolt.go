c854472cf863f02d81a2cddb8151a660ee148748
matsuwin <matsuwin@pop-os.localdomain>
2021-12-27 13:00:47 +0800

    +boltdb

--- bolt/bolt.go
+++ bolt/bolt.go
@@ -11,7 +11,7 @@ import (
 	"regexp"
 )
 
-// Select 数据检索 -> 时间范围&前缀扫描
+// Select 数据检索，支持时间范围和前缀扫描
 func Select(db *bbolt.DB, min, max int64, prefix string, limit int) ([]Element, error) {
 	if limit <= 0 {
 		limit = math.MaxInt64
@@ -41,7 +41,7 @@ func Select(db *bbolt.DB, min, max int64, prefix string, limit int) ([]Element, 
 	return data, nil
 }
 
-// SetAll 批量写入数据 -> value != nil ? insert : delete
+// SetAll 批量写入数据，value != nil ? insert : delete
 func SetAll(db *bbolt.DB, elements []Element) (_ error) {
 	update := func(tx *bbolt.Tx) (err error) {
 		b := tx.Bucket(bucket)
@@ -87,6 +87,13 @@ func New(dbname string) *bbolt.DB {
 	return db
 }
 
+type Element struct {
+	Index string
+	Value []byte
+}
+
+var bucket = []byte("bucket")
+
 func ValuesByMap(data map[string][]byte) []Element {
 	elements := make([]Element, 0, len(data))
 	for index, value := range data {
@@ -95,9 +102,45 @@ func ValuesByMap(data map[string][]byte) []Element {
 	return elements
 }
 
-type Element struct {
-	Index string
-	Value []byte
+func Get(db *bbolt.DB, index string) (value []byte, _ error) {
+	view := func(tx *bbolt.Tx) error {
+		value = tx.Bucket(bucket).Get([]byte(index))
+		return nil
+	}
+	if err := db.View(view); err != nil {
+		return nil, errors.New(err.Error())
+	}
+	return
 }
 
-var bucket = []byte("bucket")
+func GetKeyAll(db *bbolt.DB) ([]string, error) {
+	keys := make([]string, 0, 100)
+	view := func(tx *bbolt.Tx) error {
+		b := tx.Bucket(bucket)
+		return b.ForEach(func(k, v []byte) error {
+			keys = append(keys, string(k))
+			return nil
+		})
+	}
+	if err := db.View(view); err != nil {
+		return nil, errors.New(err.Error())
+	}
+	return keys, nil
+}
+
+func DeleteAll(db *bbolt.DB, keys []string) error {
+	update := func(tx *bbolt.Tx) error {
+		b := tx.Bucket(bucket)
+		for _, v := range keys {
+			err := b.Delete([]byte(v))
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	if err := db.Update(update); err != nil {
+		return errors.New(err.Error())
+	}
+	return nil
+}
